e98d2ab020d64e0aad7d35582fe963ea0a391e32
raballand.romain@gmail.com <raballand.romain@gmail.com@560f46b4-7307-2c3d-41dd-b9eae4eb4fff>
2012-11-08 10:58:33 +0000

    Commit de la premiere version du template

---
+++ trunk/template/dao/src/main/java/fr/meehome/template/dao/domain/AbstractTO.java
@@ -0,0 +1,11 @@
+package fr.meehome.template.dao.domain;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public abstract class AbstractTO implements Serializable {
+
+    public AbstractTO() {
+        super();
+    }
+}
