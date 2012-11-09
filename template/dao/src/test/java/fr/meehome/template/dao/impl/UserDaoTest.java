8db4068ee30fea70d8dc43fbe37ebf76ea589b69
raballand.romain@gmail.com <raballand.romain@gmail.com@560f46b4-7307-2c3d-41dd-b9eae4eb4fff>
2012-11-09 15:46:59 +0000

    remise d'aplomb

--- template/dao/src/test/java/fr/meehome/template/dao/impl/UserDaoTest.java
+++ template/dao/src/test/java/fr/meehome/template/dao/impl/UserDaoTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import fr.meehome.template.dao.domain.User;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/fr/meehome/template/dao/applicationContext-test.xml" })
 public class UserDaoTest extends AbstractTransactionalJUnit4SpringContextTests {
@@ -16,7 +18,41 @@ public class UserDaoTest extends AbstractTransactionalJUnit4SpringContextTests {
     private UserDao userDao;
 
     @Test
-    public void getAllPersonne() {
+    public void should_return_all_users() {
         Assert.assertEquals(2, userDao.findAll().size());
+    }
+
+    @Test
+    public void should_return_one_user_by_login() {
+        User user = new User();
+        user.setId(1);
+        user.setNom("nom1");
+        user.setPrenom("prenom1");
+        user.setLogin("login");
+        user.setPassword("password");
+
+        User userTest = userDao.searchById(1);
+
+        Assert.assertTrue(user.equals(userTest));
+    }
+
+    @Test
+    public void should_return_two_user_by_login() {
+        // TODO
+    }
+
+    @Test
+    public void should_delete_all_users_by_login() {
+        // TODO
+    }
+
+    @Test
+    public void should_delete_one_user_by_login() {
+        // TODO
+    }
+
+    @Test
+    public void should_update_one_user_by_login() {
+        // TODO
     }
 }
